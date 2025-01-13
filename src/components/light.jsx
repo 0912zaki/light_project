4b3c7ef9c0325b5f640ea0de89f03575810265f4
zakaria abdullahi <zaakpoi4523@gmail.com>
2025-01-13 12:23:21 +0300

    test2

--- src/components/light.jsx
+++ src/components/light.jsx
@@ -1,5 +1,5 @@
-import lightON from ".//assets/Nal.png"
-import lightOff from ".//assets/Laamp.png"
+import lightON from "../assets/Nal.png"
+import lightOff from "../assets/Laamp.png"
 import { useState } from "react";
 
 const Light = () => {
